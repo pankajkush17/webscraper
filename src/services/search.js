6801bf68e29046854761aa9e9879e65d95914c54
Pankaj Kushwaha <99349913+pankajkush17@users.noreply.github.com>
2025-03-21 11:43:18 +0530

    Update search.js

--- src/services/search.js
+++ src/services/search.js
@@ -1,27 +1,44 @@
 const cheerio = require('cheerio');
-const { getDataFromUrl } = require('./utils');
+const { SocksProxyAgent } = require('socks-proxy-agent');
 
-const search = async (searchValue) => {
-    const html = await getDataFromUrl(`/results.php3?sQuickSearch=yes&sName=${searchValue}`);
+const getDataFromUrl = async (url, proxyAgent) => {
+    const fetch = (await import('node-fetch')).default;
+    try {
+        const response = await fetch(`https://www.gsmarena.com${url}`, { agent: proxyAgent });
+        if (!response.ok) {
+            throw new Error(`Error fetching data from URL: ${response.statusText}`);
+        }
+        return await response.text();
+    } catch (error) {
+        console.error('Error fetching data from URL:', error);
+        throw error;
+    }
+};
 
-    const $ = cheerio.load(html);
-    const json = [];
+const searchDevices = async (searchTerm, proxyAgent) => {
+    try {
+        const html = await getDataFromUrl(`/results.php3?sQuickSearch=yes&sName=${searchTerm}`, proxyAgent);
+        const $ = cheerio.load(html);
+        const devices = [];
 
-    const devices = $('.makers').find('li');
-    devices.each((i, el) => {
-        const imgBlock = $(el).find('img');
-        json.push({
-            id: $(el).find('a').attr('href').replace('.php', ''),
-            name: $(el).find('span').html().split('<br>')
-                .join(' '),
-            img: imgBlock.attr('src'),
-            description: imgBlock.attr('title'),
+        const deviceElements = $('.makers').find('li');
+        deviceElements.each((i, el) => {
+            const imgBlock = $(el).find('img');
+            devices.push({
+                id: $(el).find('a').attr('href').replace('.php', ''),
+                name: $(el).find('span').html().split('<br>').join(' '),
+                img: imgBlock.attr('src'),
+                description: imgBlock.attr('title'),
+            });
         });
-    });
 
-    return json;
+        return devices;
+    } catch (error) {
+        console.error('Error fetching search results:', error);
+        throw error;
+    }
 };
 
 module.exports = {
-    search,
+    search: searchDevices,
 };
